bae6aac4f8b4dc6cc6ff7bcf829c94ef3e1b8ab4
niki-sal <ne.salehi@gmail.com>
2021-02-23 11:13:54 -0800

    Add player component

--- src/Human.js
+++ src/Human.js
@@ -1,10 +1,13 @@
+import Player from './Player'
 //functional Component
 function Human(props){
     return(
         <div>
             <h1>Name: {props.name}</h1>
             <h1>lives in {props.location}</h1>
+            <Player position="small Forward"/>
         </div>
+        
     )
 }
 
---
+++ src/Player.js
@@ -0,0 +1,10 @@
+function Player(props){
+    return(
+        <div>
+            <h1>Player</h1>
+            <h1>Position: {props.position}</h1>
+        </div>
+    )
+}
+
+export default Player;
